85252d9bce83bf8235f2d1f3e1b75ff7c1bd3a52
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-09-17 11:57:58 +0700

    update

--- src/Public/Page/Account/SignIn/SignIn.js
+++ src/Public/Page/Account/SignIn/SignIn.js
@@ -3,9 +3,29 @@ import "./SignIn.scss";
 import "antd/dist/antd.css";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { LoginFormPage, ProFormText } from "@ant-design/pro-components";
-import { Button, Divider, Space } from "antd";
+import accountService from "../../../Service/AccountService";
 
 function SignIn() {
+  const onFinish = async (values) => {
+    let userData = {
+      username: values.username,
+      password: values.password,
+    };
+    await accountService
+      .userLogin(userData)
+      .then((res) => {
+        console.log(res);
+        // localStorage.setItem("accessToken", res.data.accessToken);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <>
       <div id="signin-page">
@@ -16,6 +36,9 @@ function SignIn() {
           }}
         >
           <LoginFormPage
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
             backgroundImageUrl="https://gw.alipayobjects.com/zos/rmsportal/FfdJeJRQWjEeGTpqgBKj.png"
             logo="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
             title="Github"
--- src/Public/Service/AccountService.js
+++ src/Public/Service/AccountService.js
@@ -1,11 +1,17 @@
+import axios from "axios";
+import { BASE_URL_SERVER } from "../config/server";
+
 const API_ENDPOINT = {
   USER_LOGIN: "/api/v1/login",
   USER_REGISTER: "/api/v1/register",
 };
 class AccountService {
- userLogin = async () => {
-    return  
- }   
+  userLogin = async (data) => {
+    return await axios.post(BASE_URL_SERVER + API_ENDPOINT.USER_LOGIN, data);
+  };
+  userRegister = async (data) => {
+    return await axios.post(BASE_URL_SERVER + API_ENDPOINT.USER_REGISTER, data);
+  };
 }
 
 const accountService = new AccountService();
